fde28e3ea04e1d5282824a41daef054f1eccdc87
THOMAS <21305147@ppti-14-401-03.ufr-info-p6.jussieu.fr>
2023-10-06 13:03:40 +0200

    reste implémenter le end puis finir tme2 et tme3...

--- TME2/main.cpp
+++ TME2/main.cpp
@@ -7,60 +7,142 @@
 #include <forward_list>
 #include <string>
 
-
 template <typename K, typename V>
 class HashMap {
-	class Entry {
-		const K key;
-		V value;
-	};
-
-	std::vector<std::forward_list<Entry>> buckets;
-
 	public:
-		HashMap(int size) {
-			for (int i = 0; i < size; i++)
-			{
-				this->buckets.push_back(std::forward_list<Entry>());
+		class Entry {
+			public:
+				const K key;
+				V value;
+		};
+
+		typedef std::vector<std::forward_list<Entry>> buckets_t;
+
+
+		class iterator {
+		public:
+			size_t index;
+			typename std::forward_list<Entry>::iterator curr;
+
+			buckets_t & buck; // ou pointeur de hashmap
+
+			iterator(buckets_t buck, size_t index, std::forward_list<Entry>::iterator curr) : buck(buck), index(index), curr(curr) {}
+
+			// Gérer débordement ?
+			void operator++() {
+				++curr;
+				if (curr == buck[index].end()){
+					index++;
+					while(index < buck.size() && buck[index].empty())
+						index++;
+
+					if (index < buck.size())
+						curr = buck[index].begin();
+				}
 			}
+
+			Entry & operator*() {
+				return  *curr ;
+			}
+
+			bool operator!=(const iterator & other) {
+				return curr != other.curr;
+			}
+		};
+
+
+		iterator begin() {
+			if (buckets.size() == 0)
+				return end();
+
+			size_t ind = 0;
+			for (; ind < buckets.size(); ++ind) {
+				if (!buckets[ind].empty())
+					break;
+			}
+			return iterator(buckets, ind, buckets[ind].begin());
 		}
+
+
+		iterator end() {
+			// TODO : modifier
+			return iterator(buckets, 0,  buckets[0].end());
+		}
+
+		buckets_t buckets;
+
+		HashMap(size_t size) : buckets(size) {}
 
 		V* get(const K & key) {
 			size_t h = std::hash<K>()(key)  % size();
-			for (const auto & oui : buckets[h])
-			{
-				if (oui.key == key)
-				{
-					return oui.value;
-				}
-			}
-			return nullptr;
+			 for (auto & ent : buckets[h]){
+				 if (ent.key == key) {
+					 return &ent.value;
+				 }
+			 }
+			 return nullptr;
 		};
 
 		 bool put (const K & key, const V & value) {
 			 size_t h = std::hash<K>()(key) % size();
-			 if (get(key) != nullptr)
-			 {
-				for (const auto & oui : buckets[h])
-				{
-					if (oui.key == key)
-					{
-						oui.value = value;
-						return true;
-					}
-				}
+			 for (auto & ent : buckets[h]){
+				 if (ent.key == key) {
+					 ent.value = value;
+					 return true;
+				 }
 			 }
-			 else
-			 {
-				 buckets[h].emplace_front(key, value);
-				 return false;
-			 }
+			 buckets[h].emplace_front(key, value);
+			 return false;
 		 }
 
 		 size_t size() const {
 			 return buckets.size();
 		 }
+
+		 void grow() {
+			 HashMap tmp(2 * buckets.size());
+			 for (auto & b : buckets) {
+				 for (auto & e : b) {
+					 tmp.put(e.key, e.value);
+				 }
+			 }
+			 this.buckets = tmp.buckets;
+		 }
 };
+
+
+template <typename iterator>
+size_t count (iterator begin, iterator end) {
+	int i = 0;
+	for (iterator it = begin; it != end; ++it)
+	{
+		++i;
+	}
+	return i;
+}
+
+template <typename iterator, typename T>
+size_t count_if_equal (iterator begin, iterator end, const T & val)
+{
+	int i = 0;
+	for (iterator it = begin; it != end; ++it)
+	{
+		if (*it == val)
+			++i;
+	}
+	return i;
+}
+
+template <typename iterator>
+iterator prochainItListeNonVide(iterator begin, iterator end) {
+	for (auto & it = begin + 1; it != end; it++) {
+		if (*it != nullptr) {
+			return it;
+		}
+	}
+	return nullptr;
+}
+
 
 
 int main () {
@@ -69,7 +151,7 @@ int main () {
 
 	ifstream input = ifstream("./tmp/WarAndPeace.txt");
 	//vector<pair<string, int>> mots;
-	HashMap<string, int> mots;
+	HashMap<string, int> mots(20);
 
 	auto start = steady_clock::now();
 	cout << "Parsing War and Peace" << endl;
@@ -90,11 +172,11 @@ int main () {
 		//for (pair<string, int> & p : mots) {
 		// on peut faire const auto &
 		for (auto & p : mots) {
-			if (p.first == word)
+			if (p.key == word)
 			{
 				trouver = true;
 				// Incrémenter valeur
-				p.second++;
+				p.value++;
 				break;
 			}
 		}
@@ -103,7 +185,7 @@ int main () {
 		if (valeur != nullptr)
 		{
 			// Mot trouvé
-			mots.put(word, valeur + 1);
+			mots.put(word, *valeur + 1);
 		}
 		else
 		{
@@ -111,10 +193,6 @@ int main () {
 			mots.put(word, 1);
 		}
 
-		// vérifier que le mot est présent dans le vecteur
-		// TODO  : if (!trouver)
-			// TODO  : mots.push_back(std::make_pair(word, 1));
-			// meilleur : mots.emplace_back(word, 1);
 
 		// word est maintenant "tout propre"
 		if (nombre_lu % 100 == 0)
@@ -123,6 +201,14 @@ int main () {
 		nombre_lu++;
 	}
 	input.close();
+
+	// TODO :
+	/*
+	 * vector<pair<string, int>> vec;
+	 * for (Entry & e : mots {
+	 * 	vec.emplace_back(e.key, e.value
+	 * 	}
+	 * */
 
 	cout << "Finished Parsing War and Peace" << endl;
 
